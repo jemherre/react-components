d61c9729dce3ae0d940f1865fc8c5a2464685bf0
Jessica <jessica.herrera82094@gmail.com>
2018-11-10 14:35:47 -0800

    Complete Bare Minimum Requirements

--- app.js
+++ app.js
@@ -7,29 +7,49 @@
 // </div>
 // );
 
+//class component
 class GroceryList extends React.Component {
     constructor(props){
         super(props);
+        this.state ={
+            done: false
+        };
+    }
+
+    onListItemHover() {
+        this.setState({
+          done: true
+        });
+    }
+
+    offListItemHover() {
+        this.setState({
+          done: false
+        });
     }
 
     render() {
-        return(
-            <div>
-                <h3>GroceryList: </h3>
-                <GroceryListItem items={['Bread','Milk','Cheese','Eggs']}/>
-            </div>
-        );
+        // Making the style conditional on our `state` lets us 
+        // // update it based on user interactions.
+        // font-weight:bold;
+        var style = {
+            fontWeight: this.state.done ? 'bold' : 'normal'
+            // textDecoration: this.state.done ? 'line-through' : 'none'
+        };
+        return (
+            <li style={style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut={this.offListItemHover.bind(this)}>Eggs</li>
+          );
     }
 }
 
-var GroceryListItem = (props) => (
-    <ul> 
-        <li>{props.items[0]}</li>
-        <li>{props.items[1]}</li>
-        <li>{props.items[2]}</li>
-        <li>{props.items[3]}</li>
-    </ul> 
-);
+// var GroceryListItem = (props) => (
+//     <ul> 
+        
+//         <li>{props.items[1]}</li>
+//         <li>{props.items[2]}</li>
+//         <li>{props.items[3]}</li>
+//     </ul> 
+// );
 
 ReactDOM.render(
     <GroceryList />,
